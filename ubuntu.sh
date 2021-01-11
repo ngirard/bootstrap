059cbe0490c1598c043fd2098e7b893ddfeef29f
Nicolas Girard <girard.nicolas@gmail.com>
2021-01-11 16:51:50 +0100

    Add openssh-server

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,4 +6,5 @@ apt update -y
 
 apt install -y ansible python3-argcomplete python3-jmespath python3-kerberos python3-libcloud python3-selinux python3-winrm python3-xmltodict
 
+apt install -y openssh-server
 
