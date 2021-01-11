0f445491ce7802ddaad659b864dfc9431468fa0f
Nicolas Girard <girard.nicolas@gmail.com>
2021-01-11 16:20:59 +0100

    First commit

---
+++ ubuntu.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -xe
+
+apt update -y
+
+apt install -y ansible python3-argcomplete python3-jmespath python3-kerberos python3-libcloud python3-selinux python3-winrm python3-xmltodict
+
+
